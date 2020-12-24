dfda1093530303149369a21359f7747c0cd8f401
🔏 - Frantz Bazile <Frantz@kargo.com>
2020-12-23 23:53:34 -0500

    🎻 - Adding EDTx contract
    Signed-off-by: 🔏 - Frantz Bazile <Frantz@kargo.com>

--- src/contracts/EDTxToken.sol
+++ src/contracts/EDTxToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
 contract DappToken {
-    string  public name = "DApp Token";
-    string  public symbol = "DAPP";
-    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
+    string  public name = "Extreme Deals Today Token";
+    string  public symbol = "EDTx";
+    uint256 public totalSupply = 98000000000000000000000000; // 98 million tokens
     uint8   public decimals = 18;
 
     event Transfer(
