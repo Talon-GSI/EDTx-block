eb5a8dc9fd13101fd88c6e1cbf3182a08e5ca9dd
🔏 - Frantz Bazile <Frantz@kargo.com>
2020-12-23 21:11:04 -0500

    🔧 - Adding HDWallet and Infura endpoint to help with deploy smart-contracts
    Signed-off-by: 🔏 - Frantz Bazile <Frantz@kargo.com>

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,10 @@
 require('babel-register');
 require('babel-polyfill');
 
+const hdWalletProvider = require('truffle-hdwallet-provider');
+const infuraEndpoint = 'https://ropsten.infura.io/v3/f6a5cd01e59d41198b857cb44cb62e5f';
+const mnemonic = '';
+
 module.exports = {
   networks: {
     development: {
@@ -8,6 +12,14 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    ropsten: {
+      provider: () => hdWalletProvider(mnemonic, infuraEndpoint),
+      network_id: 3,
+      gas: 4400000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
