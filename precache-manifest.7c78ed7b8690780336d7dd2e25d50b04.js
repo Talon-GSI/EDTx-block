02b026fdec9eea67824187ca93839fadca67d2b0
🔏 - Frantz Bazile <Frantz@kargo.com>
2020-12-25 15:48:48 -0500

    Updates

---
+++ precache-manifest.7c78ed7b8690780336d7dd2e25d50b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "11042adb5d3763616a1e",
+    "url": "/EDTx-block/static/css/main.fb717687.chunk.css"
+  },
+  {
+    "revision": "11042adb5d3763616a1e",
+    "url": "/EDTx-block/static/js/main.11042adb.chunk.js"
+  },
+  {
+    "revision": "5fe99bce5ed257f58f0a",
+    "url": "/EDTx-block/static/css/1.be854fe7.chunk.css"
+  },
+  {
+    "revision": "5fe99bce5ed257f58f0a",
+    "url": "/EDTx-block/static/js/1.5fe99bce.chunk.js"
+  },
+  {
+    "revision": "1b5f9ec30ea3b989f618",
+    "url": "/EDTx-block/static/js/runtime~main.1b5f9ec3.js"
+  },
+  {
+    "revision": "59c38ff6243f7947f014677c3032b59d",
+    "url": "/EDTx-block/static/media/xtreme-logo.59c38ff6.jpeg"
+  },
+  {
+    "revision": "29421fe6c968d3362723aff4eacd6afe",
+    "url": "/EDTx-block/index.html"
+  }
+];
