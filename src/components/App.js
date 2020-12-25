6bb3c4dfc81f26089c0674d5c85951dca1183810
🔏 - Frantz Bazile <Frantz@kargo.com>
2020-12-25 01:11:22 -0500

    🔧 - Connecting Frontend with Blockchain data
    Signed-off-by: 🔏 - Frantz Bazile <Frantz@kargo.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,94 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import Main from './Main.js'
 import './App.css'
+import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import EDTxToken from '../abis/EDTxToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
 
 class App extends Component {
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert('Hey Bruv, Non-Ethereum browser detected.  You gotta consider getting MetaMask!')
+    }
+  }
+
+  // Actually don't know why this can be loaded out of order with above
+  async loadBlockchainData() {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts) // <- prints array to console
+    this.setState({ account: accounts[0] })
+
+    const networkId = await web3.eth.net.getId()
+    console.log(networkId) // loads network id Metamask is using
+
+    // Loading DaiToken contract
+    const daiTokenData = DaiToken.networks[networkId]
+    if (daiTokenData) {
+      const daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+      this.setState({ daiToken })
+      let daiTokenBalance = await daiToken.methods.balanceOf(this.state.account).call()
+      this.setState({ daiTokenBalance: daiTokenBalance.toString() })
+      console.log({ balance: daiTokenBalance })
+    } else {
+      window.alert('DaiToken contract not deployed to detected Network Bruv')
+    }
+
+    // Loading EDTxToken contract
+    const eDTxTokenData = EDTxToken.networks[networkId]
+    if (eDTxTokenData) {
+      const eDTxToken = new web3.eth.Contract(EDTxToken.abi, eDTxTokenData.address)
+      this.setState({ eDTxToken })
+      let eDTxTokenBalance = await eDTxToken.methods.balanceOf(this.state.account).call()
+      this.setState({ eDTxTokenBalance: eDTxTokenBalance.toString() })
+      console.log({ balance: eDTxTokenBalance })
+    } else {
+      window.alert('EDTxToken contract not deployed to detected Network Bruv')
+    }
+
+    // Loading TokenFarm contract
+    const tokenFarmData = TokenFarm.networks[networkId]
+    if (tokenFarmData) {
+      const tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+      this.setState({ tokenFarm })
+      let stakingBalance = await tokenFarm.methods.stakingBalance(this.state.account).call()
+      this.setState({ stakingBalance: stakingBalance.toString() })
+      console.log({ stakingBalance: stakingBalance })
+    } else {
+      window.alert('TokenFarm contract not deployed to detected Network Bruv')
+    }
+
+    this.setState({ loading: false })
+    console.log('Loading Complete 🤖')
+  }
 
   constructor(props) {
     super(props)
     this.state = {
-      account: '0x0'
+      // Initializing values
+      account: 'Pls install MetaMask',
+      daiToken: {},
+      eDTxToken: {},
+      tokenFarm: {},
+      // From User
+      daiTokenBalance: '0',
+      eDTxTokenBalance: '0',
+      stakingBalance: '0',
+      loading: true
     }
   }
 
@@ -26,7 +107,8 @@ class App extends Component {
                 >
                 </a>
 
-                <h1>Hello, World!</h1>
+                <Main />
+                <h1>yo pussy!</h1>
 
               </div>
             </main>
